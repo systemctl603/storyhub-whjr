3e18b5d626d33661f7b13dc17dbaee6056de410c
systemctl603 <arjunsenthilvel1@gmail.com>
2020-09-16 20:26:21 +0530

    Added search functionality

--- screens/ReadStory.js
+++ screens/ReadStory.js
@@ -1,6 +1,36 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { Appbar, Card } from "react-native-paper";
+import { SearchBar } from "react-native-elements";
+import { db } from "../firebase.conf";
 
 export default function ReadStory() {
-  return <Text>Read a story</Text>;
+  const [search, setSearch] = React.useState("");
+  const [stories, setStories] = React.useState([]);
+
+  async function handleSearch(text) {
+    setSearch(text);
+    var list = await db.collection("/stories").where("title", "==", text).get();
+    var data = [];
+    list.docs.map((doc) => {
+      data.push(doc.data());
+    });
+    console.log(data);
+    setStories(data);
+  }
+
+  return (
+    <View>
+      <SearchBar
+        placeholder="Type Here"
+        value={search}
+        onChangeText={handleSearch}
+      />
+      {stories.map((story) => (
+        <Card>
+          <Card.Title title={story.title} subtitle={`By: ${story.author}`} />
+        </Card>
+      ))}
+    </View>
+  );
 }
